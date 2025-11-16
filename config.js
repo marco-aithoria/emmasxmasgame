f3ea9453415b6beb8231f5947484ec7d0c9b39d8
Marco Viets <marco.viets@aithoria.de>
2025-11-16 01:39:00 +0100

    Add more snowman names to configuration

--- config.js
+++ config.js
@@ -5,6 +5,18 @@
 const SNOWMAN_NAMES = {
     male: [
         'Julian',
+        'Simon',
+        'Max',
+        'Leo',
+        'Felix',
+        'Noah',
+        'Lukas',
+        'Maximilian',
+        'Leon',
+        'Noah',
+        'Lukas',
+        'Maximilian',
+        'Leon',
         'Ben',
     ],
     female: [
@@ -12,6 +24,17 @@ const SNOWMAN_NAMES = {
         'Lea',
         'Isabella',
         'Lotta',
+        'Julia',
+        'Lisa',
+        'Sarah',
+        'Anna',
+        'Laura',
+        'Sophia',
+        'Maria',
+        'Anna',
+        'Laura',
+        'Sophia',
+        'Maria',
     ]
 };
 
